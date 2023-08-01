ee28d79d6b6287e9c182b9a519b5750e43e5cd5f
halilibrahimdemircan <58706397+halilibrahimdemircan@users.noreply.github.com>
2023-08-01 22:19:54 +0300

    install

---
+++ my-app/app/dashboard/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HeaderComponent from "../../../components/Header";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <HeaderComponent />
+      <div>Dashboard</div>
+    </div>
+  );
+};
+
+export default Dashboard;
